9e9889c78ca596c5ef0edb1c92de68ab65f5e1e5
leoul <leoul.emiru@gmail.com>
2021-07-12 00:35:30 -0400

    now using the latest PlaceAutoComplete API

--- app/src/main/java/com/example/placessearch/MainActivity.kt
+++ app/src/main/java/com/example/placessearch/MainActivity.kt
@@ -4,72 +4,93 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
 
+/**
+ * @author leoul
+ * custom PlaceAutoComplete. refer https://developers.google.com/maps/documentation/places/android-sdk/client-migration for more detail
+ */
 class MainActivity : AppCompatActivity() {
-    private lateinit var clearImgV: ImageView
     private lateinit var placesAdapter: PlaceAutoCompleteAdapter
+    private lateinit var clearResults: ImageView
+    private lateinit var placesRcyView: RecyclerView
+    private lateinit var placesInput: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        clearImgV = findViewById(R.id.clear)
+        Places.initialize(this, "places_api_key") // TODO add api key here, and in AndroidManifest.xml
+        val placesClient = Places.createClient(this)
+        val token = AutocompleteSessionToken.newInstance()
+        val req = FindAutocompletePredictionsRequest.builder()
+            .setCountry("US")
+            .setTypeFilter(TypeFilter.ADDRESS)
+            .setSessionToken(token)
 
-        Places.initialize(this, "add google_maps_key")
-        val mPlacesClient = Places.createClient(this)
+        clearResults = findViewById(R.id.clearImgView); clearResults.setOnClickListener(clkListener)
 
-        findViewById<RecyclerView>(R.id.placesRcyView).apply {
+        placesInput = findViewById(R.id.search_et)
+        placesInput.addTextChangedListener(txtWatcher)
+        placesInput.setOnFocusChangeListener { view, hasFocus ->
+            if (!hasFocus) {
+                hideKeyboard(view)
+            }
+        }
+
+        placesRcyView = findViewById(R.id.placesRcyView)
+        placesRcyView.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(context)
-            placesAdapter = PlaceAutoCompleteAdapter(context, mPlacesClient)
+            placesAdapter = PlaceAutoCompleteAdapter(placesClient, req, placesInput, placesRcyView)
             adapter = placesAdapter
 
         }
-        findViewById<EditText>(R.id.search_et).addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
 
+    private val txtWatcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            if (s.isNullOrBlank()) {
+                placesRcyView.visibility = View.GONE
+                clearResults.visibility = View.GONE
+            } else {
+                placesAdapter.filter.filter(s.toString())
+                placesRcyView.visibility = View.VISIBLE
+                clearResults.visibility = View.VISIBLE
             }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                if (count > 0) {
-//                    clearImgV.visibility = View.VISIBLE;
-//                    if (placesAdapter != null) {
-//                        mRecyclerView.setAdapter(mAdapter);
-//                    }
-//                } else {
-//                    clearImgV.visibility = View.GONE;
-//                    if (mSavedAdapter != null && mSavedAddressList.size() > 0) {
-//                        mRecyclerView.setAdapter(mSavedAdapter);
-//                    }
-//                }
-//                if (!s.toString().equals("") && mGoogleApiClient.isConnected()) {
-//                    mAdapter.getFilter().filter(s.toString());
-//                } else if (!mGoogleApiClient.isConnected()) {
-////                    Toast.makeText(getApplicationContext(), Constants.API_NOT_CONNECTED, Toast.LENGTH_SHORT).show();
-//                    Log.e("", "NOT CONNECTED");
-//                }
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                if (s.isNullOrBlank()) {
-                    clearImgV.visibility = View.GONE
-                } else {
-                    clearImgV.visibility = View.VISIBLE
-                    placesAdapter.filter.filter(s.toString());
-                }
-            }
-
-        })
+        }
 
     }
 
+    private val clkListener = View.OnClickListener {
+        when (it.id) {
+            R.id.clearImgView -> {
+                placesRcyView.visibility = View.GONE
+                placesInput.text.clear()
+            }
+        }
+    }
 
+    private fun hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
 
 
--- app/src/main/java/com/example/placessearch/PlaceAutoCompleteAdapter.kt
+++ app/src/main/java/com/example/placessearch/PlaceAutoCompleteAdapter.kt
@@ -1,70 +1,62 @@
 package com.example.placessearch
 
-import android.content.Context
 import android.graphics.Typeface
 import android.text.style.StyleSpan
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Filter
 import android.widget.Filterable
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.placessearch.util.getAutocomplete
+import com.google.android.gms.common.api.ApiException
 import com.google.android.libraries.places.api.model.AutocompletePrediction
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
 import com.google.android.libraries.places.api.net.PlacesClient
 
 /**
  * @author leoul.
- * TODO Enter description
+ * PlaceAutoCompleteAdapter.
+ * Refere https://developers.google.com/maps/documentation/places/android-sdk/client-migration for detail
  */
 class PlaceAutoCompleteAdapter(
-    val context: Context, val mPlacesClient: PlacesClient
+    val placesClient: PlacesClient,
+    val req: FindAutocompletePredictionsRequest.Builder,
+    val searchEditTxt: EditText,
+    val placesRcyView: RecyclerView
 ) : RecyclerView.Adapter<PlaceAutoCompleteAdapter.ViewHolder>(), Filterable {
-    private var mContext: Context = context
-   private var resultList = arrayListOf<String>()
-//    private var resultList:List<AutocompletePrediction>
-
+    private var resultList: List<AutocompletePrediction>? = null
 
     override fun getFilter(): Filter {
         return object : Filter() {
             override fun performFiltering(constraint: CharSequence?): FilterResults {
                 val filterResults = FilterResults()
                 if (constraint != null) {
-                    resultList.clear()
-                    val addressList = getAutocomplete(mPlacesClient, constraint.toString())
-                    addressList.let {
-//                        resultList = it
-                        for (i in addressList.indices) {
-                            val item = addressList[i]
-                            resultList.add(item.getFullText(StyleSpan(Typeface.BOLD)).toString())
+                    val locReq = req.setQuery(constraint.toString()).build()
 
-//                            resultList.add(PlaceDataModel(item.placeId, item.getFullText(
-//                                StyleSpan(
-//                                Typeface.BOLD)
-//                            ).toString()))
+                    placesClient.findAutocompletePredictions(locReq)
+                        .addOnSuccessListener {
+                            resultList = it.autocompletePredictions
+                            notifyDataSetChanged()
+                            filterResults.values = resultList
+                            filterResults.count = resultList?.size!!
                         }
-                    }
-                    filterResults.values = resultList
-                    filterResults.count = resultList.size
+                        .addOnFailureListener {
+                            Log.e(TAG, "Place not found: " + (it as ApiException).statusCode)
+                        }
                 }
                 return filterResults
             }
 
             override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
                 if (results != null && results.count > 0) {
-//                    context.runOnUiThread {
                     notifyDataSetChanged()
-//                    }
-                } else {
-
-                    //notifyDataSetInvalidated() // TODO ???
                 }
             }
-
         }
     }
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.place_cell, parent, false)
@@ -72,18 +64,24 @@ class PlaceAutoCompleteAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.place.text = resultList[position]
+        holder.place.text = resultList?.get(position)?.getFullText(StyleSpan(Typeface.NORMAL))
     }
 
-    override fun getItemCount() = if (resultList.isNullOrEmpty()) 0 else resultList.size
+    override fun getItemCount() = if (resultList.isNullOrEmpty()) 0 else resultList!!.size
 
     inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val place: TextView = v.findViewById(R.id.address)
 
         init {
-            v.setOnClickListener {  //  click listener for the ViewHolder's View.
-
+            place.setOnClickListener {  //  click listener for the ViewHolder's View.
+                searchEditTxt.setText(place.text)
+                searchEditTxt.clearFocus()
+                placesRcyView.visibility = View.GONE
             }
         }
+    }
+
+    companion object {
+        private const val TAG = "PlaceAutoCompleteAdapter"
     }
 }
